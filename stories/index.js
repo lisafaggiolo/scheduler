8236feff0285da80841eebadbcc267bae48b0800
lisa <lisa.faggiolo@live.ca>
2020-09-29 18:28:52 -0500

    Added Confirm, Error, Status components and their stories

--- stories/index.js
+++ stories/index.js
@@ -15,7 +15,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
-
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 
 
@@ -159,5 +160,11 @@ storiesOf("Appointment", module)
       onDelete={action("onDelete")}/>))
   .add("Confirm", () => (
     <Confirm 
+      message="delete the appointment?"
       onConfirm={action("onConfirm")}
-      onCancel={action("onCancel")}/>));
+      onCancel={action("onCancel")}/>))
+  .add("Status", () => <Status message="Deleting"/>)
+  .add("Error", () => (
+    <Error 
+      message="Could not delete appointment"
+      onClose={action("onClose")}/>));
