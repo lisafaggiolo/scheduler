257f0009d9c81ca49238f29230e72d43f86be981
lisa <lisa.faggiolo@live.ca>
2020-09-29 12:25:26 -0500

    Added State management of day in Application.jsx

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "components/Application.scss";
 //import classNames from "classnames";
 //import DayListItem from "components/DayListItem";
@@ -64,6 +64,8 @@ const days = [
 
 
 export default function Application(props) {
+  
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -76,8 +78,8 @@ export default function Application(props) {
       <nav className="sidebar__menu">
         {<DayList
           days={days}
-          day={"Monday"}
-          setDay={day => console.log(day)}
+          day={day}
+          setDay={setDay}
         />}
       </nav>
       <img
